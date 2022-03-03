e151dc5db28d08c056d880757ceede27a3dd6b94
Alex <alexespinoza11f@gmail.com>
2022-03-02 19:03:49 -0800

    Created the navbar-header design for admin's view faculty preferences page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/admin-faculty-preferences/Header';
 
 function App() {
   return (
     <div>
-      hello everyone
+      <Header />
     </div>
   );
 }
--- frontend/src/components/admin-faculty-preferences/Header.js
+++ frontend/src/components/admin-faculty-preferences/Header.js
@@ -1,1 +1,24 @@
 // This file is going to display the top nav bar of the admin faculty preferneces page
+
+import React from 'react'
+import '../../styles/adminFacultyPreferences.css'
+
+const Header = () => {
+  return (
+    <div>
+        <div className='navbar'>
+            <h1>Class Scheduler</h1>
+            <nav>
+                <ul className='nav_links'>
+                    <li><a href='#'>Update Options</a></li>
+                    <li><a href='#'>Schedule</a></li>
+                    <li><a href='#'>Preferences</a></li>
+                </ul>
+            </nav>
+        </div>
+        <hr></hr>
+    </div>
+  )
+}
+
+export default Header
