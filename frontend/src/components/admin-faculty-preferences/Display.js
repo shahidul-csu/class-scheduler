4663426f5315b978cd7d7e4530296206e183d380
Alex <alexespinoza11f@gmail.com>
2022-03-04 14:50:20 -0800

    Added 2 basic dropdowns to pick a professor and their semesters so in the end, you can get the preferences with those 2 values. The dropdowns do not have functionality that provide preferences as of yet.

--- frontend/src/components/admin-faculty-preferences/Display.js
+++ frontend/src/components/admin-faculty-preferences/Display.js
@@ -1,20 +1,38 @@
 // This page will hold both the left professors/semesters drop downs, and the preferences to accept for professors on the right side.
 
-import React from 'react'
+import React, { useState } from 'react'
 import Dropdown from './Dropdown'
 import '../../styles/adminFacultyPreferences.css'
 
 const Display = () => {
-  let professors = [{id:1, last_name: "Espinoza"}, {id:1, last_name: "Lara"}, {id:1, last_name: "Su"}]
+
+  // Dummy data
+  const [professors, setProfessors] = useState([
+    {id:1, last_name: "Espinoza"}, 
+    {id:2, last_name: "Lara"}, 
+    {id:3, last_name: "Su"},
+  ])
+  const [semesters, setSemesters] = useState([
+    {id: 1, user_id: 1,name: "fall", year: "2019"},
+    {id: 2, user_id: 2,name: "spring", year: "2021"},
+    {id: 3, user_id: 2,name: "spring", year: "2019"},
+    {id: 4, user_id: 1,name: "fall", year: "2020"},
+    {id: 5, user_id: 3,name: "fall", year: "2019"},
+    {id: 6, user_id: 3,name: "spring", year: "2022"},
+    {id: 7, user_id: 2,name: "fall", year: "2020"},
+  ])
+  
+
   return (
     <div>
         <div className='parent'>
             <div className='left'>
-                fdfd
-                <Dropdown name="Professors" professors={professors}/>
+                <Dropdown name="Professor" group={professors}/>
+                <Dropdown name="Semester" group={semesters} />
             </div>
             <div className='right'>
                 right
+                yes
             </div>
         </div>
     </div>
--- frontend/src/components/admin-faculty-preferences/Dropdown.js
+++ frontend/src/components/admin-faculty-preferences/Dropdown.js
@@ -1,22 +1,30 @@
 // This file will create a drop down to be used to create the professors and theh semesters drop down on the left of the screen. 
 
-import React from 'react'
-import '../../styles/dropdown.css'
+/*
+  NOTES
+  - have to figure out which professor was selected so you can generate all the semesters they have/had preferences
+  - have to figure out which semester was selected so you can generate all the preferences thay have/had for that semester
+  */
 
-const Dropdown = ({ name, professors }) => {
+import React from 'react'
+import '../../styles/adminFacultyPreferences.css'
+
+const Dropdown = ({ name, group, onSelected }) => {
   return (
     <div className='dropdown'>
-        <p>Professors</p>
-        {/* <form action="#"> */}
-        <div className='custom-select'>
-            <select name={name} id={name}>
-                {professors.map((professor) => (
-                    <option value={professor.id}>{professor.last_name}</option>
+        <h3>{name}</h3>
+        <form action="#">
+          <div className='custom-select'>
+              <select name={name} id={name}>
+                <option value="0">Select {name}:</option>
+                {group.map((obj) => (
+                    <option value={obj.id}>
+                      {obj.hasOwnProperty('last_name') ? obj.last_name : obj.name}
+                    </option>
                 ))}
-            </select>
-            <span className='custom-arrow'></span>
-        </div>
-        {/* </form> */}
+              </select>
+          </div>
+        </form>
     </div>
   )
 }
