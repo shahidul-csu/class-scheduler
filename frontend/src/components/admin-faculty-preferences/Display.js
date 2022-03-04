affc3c1be98e347ab34455d3b4cc454a21177278
Alex <alexespinoza11f@gmail.com>
2022-03-03 21:42:51 -0800

    Divided page into 2 columns. added a simple dropdown menu to left column.

--- frontend/src/components/admin-faculty-preferences/Display.js
+++ frontend/src/components/admin-faculty-preferences/Display.js
@@ -1,1 +1,24 @@
 // This page will hold both the left professors/semesters drop downs, and the preferences to accept for professors on the right side.
+
+import React from 'react'
+import Dropdown from './Dropdown'
+import '../../styles/adminFacultyPreferences.css'
+
+const Display = () => {
+  let professors = [{id:1, last_name: "Espinoza"}, {id:1, last_name: "Lara"}, {id:1, last_name: "Su"}]
+  return (
+    <div>
+        <div className='parent'>
+            <div className='left'>
+                fdfd
+                <Dropdown name="Professors" professors={professors}/>
+            </div>
+            <div className='right'>
+                right
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Display
--- frontend/src/components/admin-faculty-preferences/Dropdown.js
+++ frontend/src/components/admin-faculty-preferences/Dropdown.js
@@ -1,1 +1,24 @@
 // This file will create a drop down to be used to create the professors and theh semesters drop down on the left of the screen. 
+
+import React from 'react'
+import '../../styles/dropdown.css'
+
+const Dropdown = ({ name, professors }) => {
+  return (
+    <div className='dropdown'>
+        <p>Professors</p>
+        {/* <form action="#"> */}
+        <div className='custom-select'>
+            <select name={name} id={name}>
+                {professors.map((professor) => (
+                    <option value={professor.id}>{professor.last_name}</option>
+                ))}
+            </select>
+            <span className='custom-arrow'></span>
+        </div>
+        {/* </form> */}
+    </div>
+  )
+}
+
+export default Dropdown
