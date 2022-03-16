d6544d20dc9d9a49738d9f96bedf017119ba974d
Alex <alexespinoza11f@gmail.com>
2022-03-16 12:26:22 -0700

    updated models and serializers.

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -3,21 +3,33 @@ from django.contrib.auth.models import User
 
 
 class DayTime(models.Model):
+    class Meta:
+        db_table = 'day_time'
+
     day_time_id = models.AutoField(primary_key=True)
     time = models.CharField(default=None, max_length=100)
 
 
 class WeekDay(models.Model):
+    class Meta:
+        db_table = 'week_day'
+
     week_day_id = models.AutoField(primary_key=True)
     week_day = models.CharField(default=None)
 
 
 class Classroom(models.Model):
+    class Meta:
+        db_table = 'classroom'
+
     classroom_id = models.AutoField(primary_key=True)
     capacity = models.IntegerField(default=None)
 
 
 class Semester(models.Model):
+    class Meta:
+        db_table = 'semester'
+
     semester_id = models.AutoField(primary_key=True)
     year = models.IntegerField(default=None)
     name = models.CharField(default=None, max_length=100)
@@ -25,6 +37,9 @@ class Semester(models.Model):
 
 
 class ParameterData(models.Model):
+    class Meta:
+        db_table = 'parameter_data'
+
     parameter_id = models.AutoField(primary_key=True)
     approved = models.BooleanField(default=False)
     requirement = models.BooleanField(default=False)
@@ -32,76 +47,95 @@ class ParameterData(models.Model):
 
 
 class TimeSlot(models.Model):
+    class Meta:
+        db_table = 'time_slot'
+
     time_slot_id = models.AutoField(primary_key=True)
     week_day_obj = models.ForeignKey(WeekDay, null=True, default=None)
-    # week_day_objs = models.ManyToManyField(WeekDay, blank=True)
     day_time_obj = models.ForeignKey(DayTime, null=True, default=None)
-    # day_time_objs = models.ManyToManyField(WeekDay, blank=True)
 
 
 class UserGroupClassParameter(models.Model):
+    class Meta:
+        db_table = 'user_group_class_parameter'
+
     user_obj = models.ForeignKey(User, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
     parameter_obj = models.ForeignKey(ParameterData, primary_key=True, null=True, default=None)
-    # parameter_objs = models.ManyToManyField(WeekDay, blank=True)
 
 
-# should param/timeslots have many to many fields?
 class UserTimeParameter(models.Model):
+    class Meta:
+        db_table = 'user_time_parameter'
+
     user_obj = models.ForeignKey(User, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
     parameter_obj = models.ForeignKey(ParameterData, primary_key=True, null=True, default=None)
     time_slot_obj = models.ForeignKey(TimeSlot, primary_key=True, null=True, default=None)
 
 
 class Course(models.Model):
+    class Meta:
+        db_table = 'course'
+
     course_id = models.AutoField(primary_key=True)
     name = models.CharField(default=None, max_length=100)
     units = models.IntegerField(default=None)
     number_per_week = models.IntegerField(default=None)
-    # sync_time = models.DateTimeField()
+    sync_time = models.BooleanField(default=False)
     capacity = models.IntegerField(default=None)
 
 
 class Teaches(models.Model):
+    class Meta:
+        db_table = 'teaches'
+
     user_obj = models.ForeignKey(User, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
     course_obj = models.ForeignKey(Course, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
-    # course_objs = models.ManyToManyField(Course, blank=True)
     semester_obj = models.ForeignKey(Semester, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
 
 
 class Schedule(models.Model):
+    class Meta:
+        db_table = 'schedule'
+
     schedule_id = models.AutoField(primary_key=True)
     fitness_score = models.IntegerField(default=None)
 
 
 class ScheduledCourse(models.Model):
+    class Meta:
+        db_table = 'scheduled_course'
+
     schedule_obj = models.ForeignKey(Schedule, primary_key=True, null=True, default=None)
     user_obj = models.ForeignKey(User, primary_key=True, null=True, default=None)
     course_obj = models.ForeignKey(Course, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
     time_slot_obj = models.ForeignKey(TimeSlot, primary_key=True, null=True, default=None)
-    # time_slot_objs = models.ManyToManyField(TimeSlot, blank=True)
     semester_obj= models.ForeignKey(Semester, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
     classroom_obj = models.ForeignKey(Classroom, primary_key=True, null=True, default=None)
 
 
 class CourseTimeParameter(models.Model):
+    class Meta:
+        db_table = 'course_time_parameter'
+
     parameter_obj = models.ForeignKey(ParameterData, primary_key=True, null=True, default=None)
     course_obj = models.ForeignKey(Course, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
     time_slot_obj = models.ForeignKey(TimeSlot, primary_key=True, null=True, default=None)
-    # time_slot_objs =models.ManyToManyField(TimeSlot, blank=True)
 
 
 class ClassroomParameter(models.Model):
+    class Meta:
+        db_table = 'classroom_parameter'
+
     parameter_obj = models.ForeignKey(ParameterData, primary_key=True, null=True, default=None)
-    # parameter_objs = models.ManyToManyField(ParameterData, blank=True)
     course_obj = models.ForeignKey(Course, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
-    # course_objs = models.ManyToManyField(Course, blank=True)
     classroom_obj = models.ForeignKey(Classroom, primary_key=True, null=True, default=None)
 
 
 class SemesterParameter(models.Model):
-    # parameter_objs = models.ManyToManyField(ParameterData, blank=True)
+    class Meta:
+        db_table = 'semester_parameter'
+
     parameter_obj = models.ForeignKey(ParameterData, primary_key=True, null=True, default=None)
-    # course_objs = models.ManyToManyField(Course, blank=True)
     course_obj = models.ForeignKey(Course, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
     semester_obj = models.ForeignKey(Semester, primary_key=True, null=True, default=None, on_delete=models.CASCADE)
 
--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import User, DayTime, WeekDay, Classroom, Semester, ParameterData, TimeSlot, UserGroupClassParameter, UserTimeParameter, Course, Teaches, Schedule, ScheduledCourse, CourseTimeParameter, ClassroomParameter, SemesterParameter
+from .models import User, DayTime, WeekDay, Classroom, Semester, ParameterData, \
+    TimeSlot, UserGroupClassParameter, UserTimeParameter, Course, Teaches, \
+    Schedule, ScheduledCourse, CourseTimeParameter, ClassroomParameter, SemesterParameter
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -41,15 +43,12 @@ class ParameterDataSerializer(serializers.ModelSerializer):
 
 
 class TimeSlotSerializer(serializers.ModelSerializer):
-    # week_day_objs = WeekDaySerializer(many=True)
-    # day_time_objs = DayTimeSerializer(many=True)
     class Meta:
         model = TimeSlot
         fields = ['time_slot_id', 'week_day_obj', 'day_time_obj']
 
 
 class UserGroupClassParameterSerializer(serializers.ModelSerializer):
-    # parameter_objs = ParameterDataSerializer(many=True)
     class Meta:
         model = UserGroupClassParameter
         fields = ['user_obj', 'parameter_obj']
@@ -64,11 +63,10 @@ class UserTimeParameterSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ['course_id', 'name', 'units', 'number_per_week', 'capacity']
+        fields = ['course_id', 'name', 'units', 'number_per_week', 'sync_time', 'capacity']
 
 
 class TeachesSerializer(serializers.ModelSerializer):
-    # course_obj = CourseSerializer(many=True)
     class Meta:
         model = Teaches
         fields = ['user_obj', 'course_obj', 'semester_obj']
@@ -81,7 +79,6 @@ class ScheduleSerializer(serializers.ModelSerializer):
 
 
 class ScheduledCourseSerializers(serializers.ModelSerializer):
-    # time_slot_objs = TimeSlotSerializer(many=True)
     class Meta:
         model = ScheduledCourse
         fields = ['schedule_obj', 'user_obj', 'course_obj', 'time_slot_obj',
@@ -89,23 +86,18 @@ class ScheduledCourseSerializers(serializers.ModelSerializer):
 
 
 class CourseTimeParameterSerializer(serializers.ModelSerializer):
-    # time_slot_objs = TimeSlotSerializer(many=True)
     class Meta:
         model = CourseTimeParameter
         fields = ['parameter_obj', 'course_obj', 'time_slot_obj']
 
 
 class ClassroomParameterSerializer(serializers.ModelSerializer):
-    parameter_objs = ParameterDataSerializer(many=True)
-    course_objs = CourseSerializer(many=True)
     class Meta:
         model = ClassroomParameter
         fields = ['parameter_obj', 'course_obj', 'classroom_obj']
 
 
 class SemesterParameterSerializer(serializers.ModelSerializer):
-    # parameter_objs = ParameterDataSerializer(many=True)
-    # course_obj = CourseSerializer(many=True)
     class Meta:
         model = SemesterParameter
         fields = ['parameter_obj', 'course_obj', 'semester_obj']
