d9cb68c441b086fd19ad5b9330877938bdc16314
Ashley Garcia <32247665+ashgarc20@users.noreply.github.com>
2022-04-05 18:58:23 -0700

    Latest Admin Faculty Preference page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer.js'
 import LandingPage from './components/LandingPage';
+import Display from './components/admin-faculty-preferences/Display';
 
 function App() {
   return (
     <div>
       <Header />
-      <LandingPage/>
+      {/* <LandingPage/> */}
+
+      <Display/>
       <Footer/>
     </div>
   );
