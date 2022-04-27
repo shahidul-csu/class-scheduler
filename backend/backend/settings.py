2cc42cac195ef99609966ee3a799cebb4f9d8553
Alex Espinoza-Fuentes <alexespinoza11f@gmail.com>
2022-04-27 15:57:38 -0700

    configuring installed apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'ClassSchedulerBackend'
+    'ClassSchedulerBackend',
+    'models'
 ]
 
 MIDDLEWARE = [
