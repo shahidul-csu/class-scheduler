543f00ff35689074a30d77ecfa6183a5f0762d7e
Alex Espinoza-Fuentes <alexespinoza11f@gmail.com>
2022-05-09 15:48:00 -0700

    Added axios routes and requests.

--- frontend/src/components/settings/SettingsUsers.js
+++ frontend/src/components/settings/SettingsUsers.js
@@ -1,6 +1,6 @@
 /* This file is going to contain the contents for the settings page */
 
-import React from "react";
+import React, {useState} from "react";
 import "../../styles/SettingsUsers.css";
 import Nav from 'react-bootstrap/Nav';
 import Card from 'react-bootstrap/Card';
@@ -11,8 +11,28 @@ import { Link } from 'react-router-dom';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Table from 'react-bootstrap/Table';
+import axios from "axios";
+import {getUserModelConfig} from "../../network/RequestTemplates";
 
 const SettingsUsers = () =>  {
+
+    const [firstName, setFirstName] = useState("")
+    const [email, setEmail] = useState("")
+
+    const addUser = () => {
+        axios(getUserModelConfig("post", {}, {"username": firstName, "name": firstName, "email": email})).then(
+            res => {
+                console.log("created new user", res.data)
+                alert("User created")
+            }
+        ).catch(
+            err => {
+                alert(err)
+                console.log(err)
+            }
+        )
+    }
+
     return(
         <div className="SettingsUsers">
         <div className="Row">
@@ -31,21 +51,19 @@ const SettingsUsers = () =>  {
                     <div className="addUser">
                         <h1>Add User</h1>
                         <div className="submitUser">
-                        <Form>
-                        <Row>
-                            <Col xs={5}>
-                            <Form.Control placeholder="User First Name" />
-                            </Col>
-                            <Col>
-                            <Form.Control placeholder="User Email" />
-                            </Col>
-                            <Col xs="auto">
-                            <Button type="submit" className="mb-2">
-                                Submit
-                            </Button>
-                            </Col>
-                        </Row>
-                        </Form>
+                            <Form onSubmit={addUser}>
+                                <Row>
+                                    <Col xs={5}>
+                                        <Form.Control id="first_name" placeholder="User First Name" onChange={(e) => setFirstName(e.target.value)} />
+                                    </Col>
+                                    <Col>
+                                        <Form.Control id="email" placeholder="User Email" onChange={(e) => setEmail(e.target.value)} />
+                                    </Col>
+                                    <Col xs="auto">
+                                        <Button type="submit" className="mb-2" onClick={addUser}>Submit</Button>
+                                    </Col>
+                                </Row>
+                            </Form>
                         </div>
                     </div>
                     <div className="allUsers">
---
+++ frontend/src/network/RequestTemplates.js
@@ -0,0 +1,51 @@
+import ROUTER from "./Router";
+import _ from 'underscore';
+
+const parseQuery = (query) => {
+    let parsedQuery = ""
+    if (!_.isEmpty(query)) {
+        parsedQuery += "?"
+        for (const [ queryKey, queryElement ] of Object.entries(query)) {
+            parsedQuery += queryKey + "=" + queryElement + "&"
+        }
+    }
+    return parsedQuery
+}
+
+const getGenericConfig = (url, method, data) => ({
+    url: url,
+    method: method,
+    headers: {
+        "Content-Type": "application/json"
+    },
+    data: data,
+})
+
+const getAuthConfig = (url, method, data = {}, token=null) => {
+    let conf = getGenericConfig(url, method, data)
+    conf.headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Token ${token == null ? JSON.parse(localStorage.getItem('token')) : token}`
+    }
+    return conf
+}
+
+const getUserModelConfig = (method, query={}, data={}, token=null) => (
+    getAuthConfig(ROUTER.api.users + parseQuery(query), method, data, token)
+)
+
+const getClassroomModelConfig = (method, query={}, data={}, token=null) => (
+    getAuthConfig(ROUTER.api.classrooms + parseQuery(query), method, data, token)
+)
+
+const getCourseModelConfig = (method, query={}, data={}, token=null) => (
+    getAuthConfig(ROUTER.api.courses + parseQuery(query), method, data, token)
+)
+
+export {
+    getGenericConfig,
+    getAuthConfig,
+    getUserModelConfig,
+    getClassroomModelConfig,
+    getCourseModelConfig
+}
