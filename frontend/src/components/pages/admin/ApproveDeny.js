fdcfb0e51f810cf9006a6fcb72332dafdde2293b
EzequielHS05 <ehernandezsimon@outlook.com>
2023-05-12 13:24:52 -0700

    approve/deny files for admin page

--- frontend/src/components/pages/admin/ApproveDeny.js
+++ frontend/src/components/pages/admin/ApproveDeny.js
@@ -3,11 +3,14 @@ import PageCss from "../../../styles/ApproveDeny.module.css";
 import axios from "axios";
 import ROUTER from '../../../network/Router'
 import { getSemesterModelConfig, getGenericAuthModelConfig } from "../../../network/RequestTemplates";
+import DropDownGroupApproveDeny from "../../PgComponents/DropDownTables/DDGroupApproveDeny";
 
 const ApproveDeny = () => {
     const [selectedSemeterId, setSelectedSemesterId] = useState("0");
+    const [selectedInstructorId, setSelectedInstructorId] = useState("0");
     const [semesterList, setSemesterList] = useState([]);
     const [instructorList, setInstructorList] = useState([]);
+    const [isDoneFetching, setIsDoneFetching] = useState(false);
 
     const populateSemesterDropDown = () => {
         axios(getSemesterModelConfig("GET", "", {}, localStorage.getItem('token'))).then(
@@ -26,7 +29,9 @@ const ApproveDeny = () => {
     useEffect(() => {
         populateSemesterDropDown();
     }, [])
-
+    useEffect(() => {
+        setSelectedInstructorId("0");
+    }, [selectedSemeterId])
     const handleSemesterChange = (e) => {
         const semesterId = e.target.value;
         setSelectedSemesterId(semesterId);
@@ -40,6 +45,7 @@ const ApproveDeny = () => {
                 setInstructorList(res.data.data)
                 console.log(JSON.stringify(res.data.data))
             }
+
         ).catch(
             err => {
                 alert(err)
@@ -47,9 +53,9 @@ const ApproveDeny = () => {
             }
         )
     }
-
     return (
         <React.Fragment>
+
             <div id={PageCss.PageBody}>
                 <div id={PageCss.container1}>
 
@@ -60,15 +66,21 @@ const ApproveDeny = () => {
                             {semester.name + " " + semester.year}</option>)}
                     </select>
                     <br></br>
-                    <select id={PageCss.instructorSelect}>
+                    <select id={PageCss.instructorSelect} onChange={(e) => { setSelectedInstructorId(e.target.value) }} value={selectedInstructorId}>
                         <option value={0}>Select Instructor:</option>
                         {instructorList.map((instructor, index) => <option key={index} value={instructor.id}>
                             {instructor.first_name + " " + instructor.last_name}</option>)}
                     </select>
                 </div>
+                <div id={PageCss.container2}>
+                    <div className={PageCss.HeaderContainer}>
+                        <span id={PageCss.Pgheading}>Please choose whether to approve or deny.</span>
 
+                    </div>
+                    <DropDownGroupApproveDeny selectedSemesterId={selectedSemeterId}
+                        selectedInstructorId={selectedInstructorId} />
+                </div>
             </div>
-
 
         </React.Fragment>
     );
