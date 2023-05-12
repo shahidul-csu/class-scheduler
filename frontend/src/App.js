b2b1bba94a985bc70862b30b94b7d0022605f1e2
EzequielHS05 <ehernandezsimon@outlook.com>
2023-05-12 12:38:43 -0700

    added paths to created files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect,createContext, useCallback} from "react";
+import React, { useState, useEffect, createContext, useCallback } from "react";
 import './App.css';
 import Header from './components/PgComponents/Header'
 import Footer from './components/PgComponents/Footer.js'
@@ -22,11 +22,12 @@ import FacultyLandingPg from "./components/pages/faculty/FacultyLandingPg";
 import AllClassrooms from "./components/pages/admin/AllClassrooms";
 import AllCourses from "./components/pages/admin/AllCourses";
 import FacultyAvaliabiltyPg from "./components/pages/faculty/FacultyAvaliabilityPg";
+import FacultyPreferencePg from "./components/pages/faculty/FacultyPreferencePg";
 import FacultyProtectedRoutes from "./components/PgComponents/FacultyProtectedRoutes";
 import LoginHandler from "./components/PgComponents/QuickLoginHandler"; //LoginHandler-- handels login
 import AdminProtectedRoutes from "./components/PgComponents/AdminProtectedRoutes";
 import ApproveDeny from "./components/pages/admin/ApproveDeny";
-import  "./styles/Dropdown.css" //Dont remove. customizes all dropdowns in website
+import "./styles/Dropdown.css" //Dont remove. customizes all dropdowns in website
 
 
 
@@ -35,19 +36,19 @@ export const LoggedInUserContext = createContext(); //passes the user details to
 function App() {
   let currentlyLoggedInUser = JSON.parse(localStorage.getItem("LoggedInUsrData"))
 
-   const [loggedInUserData, setLoggedInUserData] = useState(currentlyLoggedInUser? 
+  const [loggedInUserData, setLoggedInUserData] = useState(currentlyLoggedInUser ?
     currentlyLoggedInUser : null); //state vars
 
-  const UpdateStateVarAndLocalStorage = (data) => { 
+  const UpdateStateVarAndLocalStorage = (data) => {
     // used to update loggedInUserData and the local storage
-    localStorage.setItem("LoggedInUsrData",  JSON.stringify(data.userData))
+    localStorage.setItem("LoggedInUsrData", JSON.stringify(data.userData))
     localStorage.setItem('token', data.token)
     localStorage.setItem('userId', data.userId)
-    
+
     setLoggedInUserData(data.userData)
   }
 
-  const ClearStateVarAndLocalStorage= () => { 
+  const ClearStateVarAndLocalStorage = () => {
     // function called during logout
     localStorage.clear('LoggedInUsrData')
     localStorage.clear('token')
@@ -60,45 +61,45 @@ function App() {
 
 
       <LoggedInUserContext.Provider value={loggedInUserData}>
-      <Header logoutFunc={ClearStateVarAndLocalStorage}/>
+        <Header logoutFunc={ClearStateVarAndLocalStorage} />
         <BrowserRouter>
           <Routes>
             <Route element={<LoginHandler></LoginHandler>}>
-              <Route path="/" element={<LandingPage updateLoggedInUserData = {UpdateStateVarAndLocalStorage}></LandingPage>}></Route>
-              </Route>
-              <Route path="/settingsUser" element={<SettingsUsers></SettingsUsers>}></Route>
+              <Route path="/" element={<LandingPage updateLoggedInUserData={UpdateStateVarAndLocalStorage}></LandingPage>}></Route>
+            </Route>
+            <Route path="/settingsUser" element={<SettingsUsers></SettingsUsers>}></Route>
 
-              <Route element={<AdminProtectedRoutes></AdminProtectedRoutes>}>
-                {/* Must be logged in as a admin to view this pages. */}
+            <Route element={<AdminProtectedRoutes></AdminProtectedRoutes>}>
+              {/* Must be logged in as a admin to view this pages. */}
 
-                <Route path="/adminpage" element={<AdminPage clearLoginData={ClearStateVarAndLocalStorage}/>}></Route>
+              <Route path="/adminpage" element={<AdminPage clearLoginData={ClearStateVarAndLocalStorage} />}></Route>
 
-                <Route path="/addclass" element={<AddClass/>}></Route>
-                <Route path="/usermanagement" element={<UserManagement/>}></Route>
-                <Route path="/allclassrooms" element={<AllClassrooms/>}></Route>
-                <Route path="/allcourses" element={<AllCourses/>}></Route>
-                {/* <Route path="/data" element={<Data></Data>}></Route> */}
-                <Route path="/addUser" element={<AddUserPg></AddUserPg>}></Route>
-                <Route path="/addCourse2" element={<AddCoursePg></AddCoursePg>}></Route>
-                <Route path="/genschedule" element={<GenSchedule></GenSchedule>}></Route>
-                <Route path="/CourseClassroom" element={<CourseClassroom></CourseClassroom>}></Route>
-                <Route path="/InstructorCourse" element={<InstructorCourse></InstructorCourse>}></Route>
-                <Route path="/CourseTime" element={<CourseTime></CourseTime>}></Route>
-                <Route path="/AddSemester" element={<AddSemester></AddSemester>}></Route>
-                <Route path="/CourseSemester" element={<CourseSemester></CourseSemester>}></Route>
-                <Route path="/ApproveDeny" element ={<ApproveDeny></ApproveDeny>}></Route>
-              </Route>
+              <Route path="/addclass" element={<AddClass />}></Route>
+              <Route path="/usermanagement" element={<UserManagement />}></Route>
+              <Route path="/allclassrooms" element={<AllClassrooms />}></Route>
+              <Route path="/allcourses" element={<AllCourses />}></Route>
+              {/* <Route path="/data" element={<Data></Data>}></Route> */}
+              <Route path="/addUser" element={<AddUserPg></AddUserPg>}></Route>
+              <Route path="/addCourse2" element={<AddCoursePg></AddCoursePg>}></Route>
+              <Route path="/genschedule" element={<GenSchedule></GenSchedule>}></Route>
+              <Route path="/CourseClassroom" element={<CourseClassroom></CourseClassroom>}></Route>
+              <Route path="/InstructorCourse" element={<InstructorCourse></InstructorCourse>}></Route>
+              <Route path="/CourseTime" element={<CourseTime></CourseTime>}></Route>
+              <Route path="/AddSemester" element={<AddSemester></AddSemester>}></Route>
+              <Route path="/CourseSemester" element={<CourseSemester></CourseSemester>}></Route>
+              <Route path="/ApproveDeny" element={<ApproveDeny></ApproveDeny>}></Route>
+            </Route>
 
-              <Route element={<FacultyProtectedRoutes></FacultyProtectedRoutes>}>
-                {/* Must be logged in as a faculty to view this pages. */}
+            <Route element={<FacultyProtectedRoutes></FacultyProtectedRoutes>}>
+              {/* Must be logged in as a faculty to view this pages. */}
               <Route path="/FacultyLandingPg" element={<FacultyLandingPg clearLoginData={ClearStateVarAndLocalStorage}></FacultyLandingPg>}></Route>
               <Route path="/FacultyAvaliability" element={<FacultyAvaliabiltyPg></FacultyAvaliabiltyPg>}></Route>
-
-              </Route>
+              <Route path="/FacultyPreference" element={<FacultyPreferencePg></FacultyPreferencePg>}></Route>
+            </Route>
           </Routes>
         </BrowserRouter>
-        </LoggedInUserContext.Provider>
-      <Footer/>
+      </LoggedInUserContext.Provider>
+      <Footer />
     </div>
   );
 }
