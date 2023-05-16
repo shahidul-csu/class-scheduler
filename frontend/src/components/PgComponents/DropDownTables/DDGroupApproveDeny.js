e47d57d8f2cd3e43ee2319e6a2b61bcd291be0cd
EzequielHS05 <ehernandezsimon@outlook.com>
2023-05-16 11:24:47 -0700

    Fixing logic on how to retrieve data and style changes
    -availability retrieval
    -preference retrieval
    -changes to preference style table

--- frontend/src/components/PgComponents/DropDownTables/DDGroupApproveDeny.js
+++ frontend/src/components/PgComponents/DropDownTables/DDGroupApproveDeny.js
@@ -1,35 +1,39 @@
 import React, { createContext, useEffect, useRef, useState } from 'react';
 import PageCss from "../../../styles/ApproveDeny.module.css";
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 import ROUTER from '../../../network/Router';
 import { getGenericAuthModelConfig } from '../../../network/RequestTemplates';
 import DropDownAvailability from './DropDownAvailability';
 import DropDownPreference from './DropDownPreference';
+import loadingImg from '../../../images/loading.png'
 export const DataContext = createContext()
 
-const DefaultTimeSlots = () => [
+const AvailabilityTimeSlots = () => [
     { timeSlotName: '8AM-10AM', selected: false }, { timeSlotName: '10AM-12PM', selected: false },
     { timeSlotName: '12PM-2PM', selected: false }, { timeSlotName: '2PM-4PM', selected: false },
     { timeSlotName: '4PM-6PM', selected: false }, { timeSlotName: '6PM-8PM', selected: false }]
+const PreferenceTimeSlots = () => [
+    { timeSlotName: '8AM-10AM', score: 0 }, { timeSlotName: '10AM-12PM', score: 0 },
+    { timeSlotName: '12PM-2PM', score: 0 }, { timeSlotName: '2PM-4PM', score: 0 },
+    { timeSlotName: '4PM-6PM', score: 0 }, { timeSlotName: '6PM-8PM', score: 0 }]
 
 const DropDownGroupApproveDeny = (props) => {
-
+    const [showLoadingIcon, setShowLoadingIcon] = useState(false)
     const [isDoneFetching, setIsDoneFetching] = useState(false);
-    const [preferenceTimeSlotGroup, setPreferenceTimeSlotGroup] = useState([
-        { timeSlotGroup: DefaultTimeSlots() }, //Monday
-        { timeSlotGroup: DefaultTimeSlots() }, //Tuesday
-        { timeSlotGroup: DefaultTimeSlots() }, //Wednesday
-        { timeSlotGroup: DefaultTimeSlots() }, //Thursday
-        { timeSlotGroup: DefaultTimeSlots() }, //Friday
-    ]);
     const [availabilityTimeSlotGroup, setAvailabilityTimeSlotGroup] = useState([
-        { timeSlotGroup: DefaultTimeSlots() }, //Monday
-        { timeSlotGroup: DefaultTimeSlots() }, //Tuesday
-        { timeSlotGroup: DefaultTimeSlots() }, //Wednesday
-        { timeSlotGroup: DefaultTimeSlots() }, //Thursday
-        { timeSlotGroup: DefaultTimeSlots() }, //Friday
+        { timeSlotGroup: AvailabilityTimeSlots() }, //Monday
+        { timeSlotGroup: AvailabilityTimeSlots() }, //Tuesday
+        { timeSlotGroup: AvailabilityTimeSlots() }, //Wednesday
+        { timeSlotGroup: AvailabilityTimeSlots() }, //Thursday
+        { timeSlotGroup: AvailabilityTimeSlots() }, //Friday
+    ]);
+    const [preferenceTimeSlotGroup, setPreferenceTimeSlotGroup] = useState([
+        { timeSlotGroup: PreferenceTimeSlots() }, //Monday
+        { timeSlotGroup: PreferenceTimeSlots() }, //Tuesday
+        { timeSlotGroup: PreferenceTimeSlots() }, //Wednesday
+        { timeSlotGroup: PreferenceTimeSlots() }, //Thursday
+        { timeSlotGroup: PreferenceTimeSlots() }, //Friday
     ]);
 
     const [parameterIdsPreference, setParameterIdsPreference] = useState([]);
@@ -40,55 +44,41 @@ const DropDownGroupApproveDeny = (props) => {
     const preferenceApprovalStatus = useRef(null);
 
     useEffect(async () => {
-        setIsDoneFetching(false);
-
-        //wait for the semesterId and instructorId to be selected
-        if (props.selectedSemesterId != "0" && props.selectedInstructorId != "0") {
-            FetchExistingData();
-        }
-
-    }, [props.selectedInstructorId, props.selectedSemesterId]);
-    //Fetch entries for availability and preference using the semesterId and instructorId
-    const FetchExistingData = async () => {
-
-        console.log("Fetching data for semesterId: " + props.selectedSemesterId + " and instructorId: " + props.selectedInstructorId);
-        //fetch availability data
-        await axios(getGenericAuthModelConfig("GET", {
-            'semesterId': props.selectedSemesterId,
-            'userId': props.selectedInstructorId
-        }, {},
-            localStorage.getItem('token'), ROUTER.api.getAvaliabilityData)).then(
-                res => {
-                    if (res.data.data.length > 0) {
-                        availabilityApprovalStatus.current = res.data.data[0].approved;
-                        setParameterIdAvailability(res.data.data[0].parameter_id);
-                        updateAvailabilityVisual(res.data.data);
-
-                    } else {
-                        setParameterIdAvailability(null);
-                        availabilityApprovalStatus.current = null;
-                        resetAvailabilityTimeSlots(); //this is used to rerender the table
+        //fetch availability timeslots
+        async function fetchData() {
+            //fetch availability timeslots
+            setShowLoadingIcon(true);
+            await axios(getGenericAuthModelConfig("GET", {
+                'semesterId': props.selectedSemesterId,
+                'id': props.selectedInstructorId
+            }, {},
+                localStorage.getItem('token'), ROUTER.api.getAvaliabilityData)).then(
+                    res => {
+                        if (res.data.data.length > 0) {
+                            availabilityApprovalStatus.current = res.data.data[0].approved;
+                            setParameterIdAvailability(res.data.data[0].parameter_id);
+                            updateAvailabilityVisual(res.data.data);
+                        } else {
+                            availabilityApprovalStatus.current = null;
+                            setParameterIdAvailability(null);
+                            resetAvailabilityTimeSlots(); //this is to empty previous info and rerender table
+                        }
                     }
-                }
-            ).catch(
-                err => {
-                    alert(err)
-                    console.log(err)
-                }
-            );
-        //fetch preference timeslots 
-        await axios(getGenericAuthModelConfig("GET", {
-            'semesterId': props.selectedSemesterId,
-            'userId': props.selectedInstructorId
-        }, {},
-            localStorage.getItem('token'), ROUTER.api.getPreferenceData)).then(
+                ).catch(
+                    err => {
+                        alert(err)
+                        console.log(err)
+                    }
+                );
+            //fetch preference timeslots
+            await axios(getGenericAuthModelConfig("GET", { 'semesterId': props.selectedSemesterId, 'id': props.selectedInstructorId }, {}, localStorage.getItem('token'), ROUTER.api.getPreferenceData)).then(
                 res => {
                     if (res.data.data.length > 0) {
                         preferenceApprovalStatus.current = res.data.data[0].approved;
                         updatePreferenceVisual(res.data.data);
                     } else {
                         preferenceApprovalStatus.current = null;
-                        resetPreferenceTimeSlots(); //this is used to rerender the table 
+                        resetPreferenceTimeSlots(); //this is to empty previous info and rerender table
                     }
                 }
             ).catch(
@@ -97,72 +87,102 @@ const DropDownGroupApproveDeny = (props) => {
                     console.log(err)
                 }
             );
-        //fetch parameterIds for preference
-        let newMediumScoreParameterId = null;
-        let newHighScoreParameterId = null;
-        let parameterIdsArray = [];
-        await axios(getGenericAuthModelConfig("GET", { 'semesterId': props.selectedSemesterId, 'userId': props.selectedInstructorId }, {}, localStorage.getItem('token'), ROUTER.api.getPreferenceParameterIds)).then(
-            res => {
-                if (res.data.medium.length > 0) {
-                    newMediumScoreParameterId = res.data.medium[0].parameter_id;
+            //fetch parameterIds for preference
+            let newLowScoreParameterId = null;
+            let newMediumScoreParameterId = null;
+            let newHighScoreParameterId = null;
+            let parameterIdsArray = [];
+            await axios(getGenericAuthModelConfig("GET", { 'semesterId': props.selectedSemesterId, 'id': props.selectedInstructorId }, {}, localStorage.getItem('token'), ROUTER.api.getPreferenceParameterIds)).then(
+                res => {
+                    if (res.data.medium.length > 0) {
+                        newMediumScoreParameterId = res.data.medium[0].parameter_id;
+                    }
+                    if (res.data.high.length > 0) {
+                        newHighScoreParameterId = res.data.high[0].parameter_id;
+                    }
+                    if (res.data.low.length > 0) {
+                        newLowScoreParameterId = res.data.low[0].parameter_id;
+                    }
+                    setShowLoadingIcon(false);
                 }
-                if (res.data.high.length > 0) {
-                    newHighScoreParameterId = res.data.high[0].parameter_id;
+            ).catch(
+                err => {
+                    alert('An error has occurred, please refresh the page and try again')
+                    console.log(err)
                 }
+            )
+            //save non null parameterIds to array
+            if (newLowScoreParameterId != null) {
+                parameterIdsArray.push(newLowScoreParameterId);
             }
-        ).catch(
-            err => {
-                alert('An error has occurred, please refresh the page and try again')
-                console.log(err)
+            if (newMediumScoreParameterId != null) {
+                parameterIdsArray.push(newMediumScoreParameterId);
             }
-        )
-        if (newMediumScoreParameterId != null) {
-            parameterIdsArray.push(newMediumScoreParameterId);
-        }
-        if (newHighScoreParameterId != null) {
-            parameterIdsArray.push(newHighScoreParameterId);
-        }
-        console.log(parameterIdsArray);
-        setParameterIdsPreference(parameterIdsArray);
-        setIsDoneFetching(true);
-
-    }
-
-
-    const updatePreferenceVisual = async (data) => {
-        let newtimeSlotGroupList = [
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-        ];
-
-        for (var x = 0; x < data.length; x++) {
-            newtimeSlotGroupList[data[x].week_day_id - 1].
-                timeSlotGroup[data[x].day_time_id - 1].selected = true;
+            if (newHighScoreParameterId != null) {
+                parameterIdsArray.push(newHighScoreParameterId);
+            }
+            setParameterIdsPreference(parameterIdsArray);
+            setIsDoneFetching(true);
         }
 
-
-        setPreferenceTimeSlotGroup(newtimeSlotGroupList);
-    }
-
-    const updateAvailabilityVisual = (data) => {
-        let newtimeSlotGroupList = [
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-        ];
-
-
-        for (var x = 0; x < data.length; x++) {
-            newtimeSlotGroupList[data[x].week_day_id - 1].
-                timeSlotGroup[data[x].day_time_id - 1].selected = true;
+        //update availibility visual
+        function updateAvailabilityVisual(data) {
+            let newTimeSlotGroupList = [
+                { timeSlotGroup: AvailabilityTimeSlots() },
+                { timeSlotGroup: AvailabilityTimeSlots() },
+                { timeSlotGroup: AvailabilityTimeSlots() },
+                { timeSlotGroup: AvailabilityTimeSlots() },
+                { timeSlotGroup: AvailabilityTimeSlots() },];
+            for (let x = 0; x < data.length; x++) {
+                newTimeSlotGroupList[data[x].week_day_id - 1].timeSlotGroup[data[x].day_time_id - 1].selected = true;
+            }
+            setAvailabilityTimeSlotGroup(newTimeSlotGroupList);
         }
-        setAvailabilityTimeSlotGroup(newtimeSlotGroupList);
-    }
+        //update preference visual
+        function updatePreferenceVisual(data) {
+            let newTimeSlotGroupList = [
+                { timeSlotGroup: PreferenceTimeSlots() },
+                { timeSlotGroup: PreferenceTimeSlots() },
+                { timeSlotGroup: PreferenceTimeSlots() },
+                { timeSlotGroup: PreferenceTimeSlots() },
+                { timeSlotGroup: PreferenceTimeSlots() },];
+            for (let x = 0; x < data.length; x++) {
+                newTimeSlotGroupList[data[x].week_day_id - 1].timeSlotGroup[data[x].day_time_id - 1].score = data[x].score;
+            }
+            setPreferenceTimeSlotGroup(newTimeSlotGroupList);
+        }
+
+        //reset availability timeslots if there is no availability entries
+        function resetAvailabilityTimeSlots() {
+            let newtimeSlotGroupList = [
+                { timeSlotGroup: AvailabilityTimeSlots() },
+                { timeSlotGroup: AvailabilityTimeSlots() },
+                { timeSlotGroup: AvailabilityTimeSlots() },
+                { timeSlotGroup: AvailabilityTimeSlots() },
+                { timeSlotGroup: AvailabilityTimeSlots() },
+            ];
+            setAvailabilityTimeSlotGroup(newtimeSlotGroupList);
+        }
+
+        //reset preference timeslots if there is no preference entries
+        function resetPreferenceTimeSlots() {
+            let newtimeSlotGroupList = [
+                { timeSlotGroup: PreferenceTimeSlots() },
+                { timeSlotGroup: PreferenceTimeSlots() },
+                { timeSlotGroup: PreferenceTimeSlots() },
+                { timeSlotGroup: PreferenceTimeSlots() },
+                { timeSlotGroup: PreferenceTimeSlots() },
+            ];
+            setPreferenceTimeSlotGroup(newtimeSlotGroupList);
+        }
+        setIsDoneFetching(false);
+        //wait for instructorId to be selected
+        if (props.selectedInstructorId != "0") {
+            fetchData();
+        }
+
+    }, [props.selectedInstructorId]);
+
     //disable dropdown if data is not fetched or if semesterId and instructorId are not selected
     const isDisabled = () => {
         if (!isDoneFetching) {
@@ -171,46 +191,32 @@ const DropDownGroupApproveDeny = (props) => {
             return PageCss.notDisabled;
         }
     }
-
-
-    const resetPreferenceTimeSlots = () => {
-        let newtimeSlotGroupList = [
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-        ];
-        setPreferenceTimeSlotGroup(newtimeSlotGroupList);
-    }
-    const resetAvailabilityTimeSlots = () => {
-        let newtimeSlotGroupList = [
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-            { timeSlotGroup: DefaultTimeSlots() },
-        ];
-        setAvailabilityTimeSlotGroup(newtimeSlotGroupList);
+    const loadingIconVisibility = () => {
+        if (showLoadingIcon) {
+            return <img id={PageCss.loadingData} src={loadingImg}></img>
+        }
     }
     return (
-        <DataContext.Provider value={{
-            preferenceTimeSlots: preferenceTimeSlotGroup,
-            availabilityTimeSlots: availabilityTimeSlotGroup,
-            availabilityParameterId: parameterIdAvailability,
-            availabilityStatus: availabilityApprovalStatus,
-            preferenceParameterIds: parameterIdsPreference,
-            preferenceStatus: preferenceApprovalStatus,
+        <React.Fragment>
+            <DataContext.Provider value={{
+                preferenceTimeSlots: preferenceTimeSlotGroup,
+                availabilityTimeSlots: availabilityTimeSlotGroup,
+                availabilityParameterId: parameterIdAvailability,
+                availabilityStatus: availabilityApprovalStatus,
+                preferenceParameterIds: parameterIdsPreference,
+                preferenceStatus: preferenceApprovalStatus,
 
-        }
-        }>
-            <React.Fragment>
-                <div className={`${PageCss.dropDownShell} ${isDisabled()}`}>
-                    <DropDownAvailability entryTypeName="Availability" />
-                    <DropDownPreference entryTypeName="Preference" />
+            }
+            }>
+                <div style={{ position: 'relative' }}>
+                    {loadingIconVisibility()}
+                    <div className={`${PageCss.dropDownShell} ${isDisabled()}`}>
+                        <DropDownAvailability entryTypeName="Availability" />
+                        <DropDownPreference entryTypeName="Preference" />
+                    </div>
                 </div>
-            </React.Fragment>
-        </DataContext.Provider>
+            </DataContext.Provider>
+        </React.Fragment>
 
     );
 }
